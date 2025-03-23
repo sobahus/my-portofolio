23c9dc1cdcafd4f54527cc8ca2c6a4fc808507da
sobahus <sobahusn27@gmail.com>
2025-03-23 15:16:52 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./assets/styles/globals.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center">
+        <h1 className="animate-bounce text-5xl font-bold font-mono">
+          Hello World!
+        </h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
